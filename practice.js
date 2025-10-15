6eab7b408cb9e4c8078f261486cf217964ed80f8
unknown <aliasgar.codesymphony.com>
2025-10-15 17:43:32 +0530

    js updated

--- practice.js
+++ practice.js
@@ -29,32 +29,32 @@ if (objectFalse) {
   console.log("This will run, because objects are truthy");
 }
 
-var fruitType = "Apples";
-switch (fruitType) {
-  case "Oranges":
-    console.log("Oranges are $0.59 a pound.");
-    break;
-  case "Apples":
-    console.log("Apples are $0.32 a pound.");
-    break;
-  case "Bananas":
-    console.log("Bananas are $0.48 a pound.");
-    break;
-  case "Cherries":
-    console.log("Cherries are $3.00 a pound.");
-    break;
-  case "Mangoes":
-    console.log("Mangoes are $0.56 a pound.");
-    break;
-  case "Papayas":
-    console.log("Papayas are $2.79 a pound.");
-    break;
-  default:
-    console.log(`Sorry, we are out of ${fruitType}.`);
-}
-console.log("Is there anything else you'd like?");
+// var fruitType = "Apples";
+// switch (fruitType) {
+//   case "Oranges":
+//     console.log("Oranges are $0.59 a pound.");
+//     break;
+//   case "Apples":
+//     console.log("Apples are $0.32 a pound.");
+//     break;
+//   case "Bananas":
+//     console.log("Bananas are $0.48 a pound.");
+//     break;
+//   case "Cherries":
+//     console.log("Cherries are $3.00 a pound.");
+//     break;
+//   case "Mangoes":
+//     console.log("Mangoes are $0.56 a pound.");
+//     break;
+//   case "Papayas":
+//     console.log("Papayas are $2.79 a pound.");
+//     break;
+//   default:
+//     console.log(`Sorry, we are out of ${fruitType}.`);
+// }
+// console.log("Is there anything else you'd like?");
 
-let num = 123.4;
+/* let num = 123.4;
 console.log("\n" + num.toPrecision(8));
 
 console.log(typeof 5);
@@ -67,4 +67,112 @@ let z = parseInt(b);
 
 let c =y+z;
 
-console.log(c);
+console.log(c); */
+
+/* for (var i = 0; i < 3; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 1000);
+}
+ console.log("Loop finished"); 
+  */
+
+console.log(3 + 4);
+console.log(3 + 4 + "Hii");
+console.log("Hii" + 3 + 4);
+
+console.log("hello" && "world");
+console.log("hello" || "world");
+console.log(!"hello");
+
+console.log(5 && 4);
+console.log(5 || 4);
+console.log(!5);
+
+let user;
+let displayName = user ?? "Amit"; // ?? operator define default value to variable
+console.log(displayName);
+
+console.log(undefined ?? "default value");
+console.log(null ?? "default value");
+console.log("Raj" ?? "default value");
+
+let d;
+d ??= 10;
+console.log(d);
+
+console.log("\n");
+
+console.log(Math.min(..."12345"));
+console.log(Math.max(..."12345"));
+
+console.log(5 !== "5");
+console.log(8 !== "5");
+
+console.log("A" > "B");
+
+console.log("2" < "12");
+
+const cars = ["BMW", "Volvo", "Mercides", "Thar"];
+
+/* for(let i=0; i<cars.length; i++){
+  console.log(cars[i]);
+} */
+
+/*   let i=0;
+while(i<cars.length){
+  console.log(cars[i]);
+  i++;
+}
+ */
+
+let i = 0;
+do {
+  if (i == 2) {
+    break;
+  }
+  console.log(cars[i]);
+  i++;
+} while (i < cars.length);
+
+// Arrow functions
+let multiply = (a, b) => a * b;
+console.log(multiply(12, 2));
+
+// Objects
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: "31",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+console.log(person.fullName());
+
+/* for (var j = 0; j < 3; j++) {
+  (function (i) {
+    setTimeout(function () {
+      console.log(i);
+    }, 1000);
+  })(j);
+}
+console.log("Loop finished");*/
+
+
+let s = "Hello";
+
+for(const i of s){
+  console.log(i);
+}
+
+const fruits = {
+  "apple":"red",
+  "orange":"orange",
+  "banana":"yellow"
+};
+
+for(const i in fruits){
+  console.log(i+" : "+fruits[i]);
+}
