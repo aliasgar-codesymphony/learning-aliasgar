3a3cf2ff15511750257c65180902ce429eb8062d
unknown <aliasgar.codesymphony.com>
2025-10-18 15:51:10 +0530

    js updated

--- site.js
+++ site.js
@@ -5,7 +5,7 @@ const products = [
     image:
       "https://img.freepik.com/free-psd/close-up-delicious-apple_23-2151868338.jpg?semt=ais_hybrid&w=740&q=80",
     price: 10,
-    qty: 0,
+    qty: 1,
     total: 0,
   },
   {
@@ -13,7 +13,7 @@ const products = [
     name: "Mango",
     image: "https://kokankanya.com/wp-content/uploads/2023/04/Hapus_Mango.jpg",
     price: 15,
-    qty: 0,
+    qty: 1,
     total: 0,
   },
   {
@@ -22,51 +22,45 @@ const products = [
     image:
       "https://media.istockphoto.com/id/164142758/photo/single-pear.jpg?s=612x612&w=0&k=20&c=bTXnXhqVLSsAqX6UVdNuhuXS_U3XvHwZ1NpIgl95S6c=",
     price: 6,
-    qty: 0,
+    qty: 1,
     total: 0,
   },
 ];
 
+let increment = (index) => {
+  products[index].qty++;
+  console.log(products[index].qty);
+  renderProducts();
+};
+let decrement = (index) => {
+  if (products[index].qty > 1) {
+    products[index].qty--;
+    renderProducts();
+  }
+};
 const tableBody = document.querySelector("#myTable tbody");
 
 function renderProducts() {
   tableBody.innerHTML = "";
-  products.forEach((i) => {
+  products.forEach((i, index) => {
     const newRow = document.createElement("tr");
     newRow.innerHTML = `
-   <tr>
+  <tr>
           <td>${i.id}</td>
           <td >${i.name}</td>
           <td><img src="${i.image}"></td>
           <td>${i.price}</td>
-          <td><button class="minus" data-id="${i.id}">-</button><label class="lblqty">${i.qty}</label><button class="plus" data-id="${i.id}">+</button></td>
-          <td>${i.total}</td>
+          <td><button class="minus" onclick="decrement(${index})">-</button><label class="lblqty">${
+      i.qty
+    }</label><button class="plus" data-id="${
+      i.id
+    }" id="plus" onclick="increment(${index})">+</button></td>
+          <td>${i.price * i.qty}</td>
     </tr>
 `;
+    console.log(newRow);
     tableBody.appendChild(newRow);
   });
 }
 
 renderProducts();
-
-tableBody.addEventListener("click", (event) => {
-  const target = event.target;
-  console.log(target);
-  console.log(target.classList);
-  if (target.classList.contains("plus") || target.classList.contains("minus")) {
-    const productId = parseInt(target.getAttribute("data-id"));
-    const product = products.find((p) => p.id === productId);
-
-    if (product) {
-      if (target.classList.contains("plus")) {
-        product.qty++;
-        product.total = product.qty * product.price;
-      } else if (target.classList.contains("minus") && product.qty > 0) {
-        product.qty--;
-        product.total = product.qty * product.price;
-      }
-      //console.log(product.qty*product.price);
-      renderProducts();
-    }
-  }
-});
